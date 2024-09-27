5364fb59a3954b1425fad9fc50262f99b10b1de6
NithishCIDC <nithish.sakthivelmurugan@concertidc.com>
2024-09-27 20:07:39 +0530

    Add project files.

---
+++ Store/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Store.Data;
+using Store.Model;
+
+namespace Store.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public ProductsController(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		public ActionResult<IEnumerable<Products>> GetAll()
+		{
+			var List = _dbContext.Products.ToList();
+			if (List is not null)
+			{
+				return Ok(List);
+			}
+			return NoContent();
+		}
+
+		[HttpGet("{id:Guid}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		public ActionResult<Products> GetById(Guid id)
+		{
+			var pro = _dbContext.Products.Find(id)!;
+			if (pro is not null)
+			{
+				return Ok(pro);
+			}
+			return NoContent();
+		}
+
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		public async Task<ActionResult<Products>> Create([FromBody] Products products)
+		{
+			await _dbContext.Products.AddAsync(products);
+			await _dbContext.SaveChangesAsync();
+			return CreatedAtAction("GetById",new {Id=products.Id },products);
+		}
+
+		[HttpPut]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult> Update([FromBody] Products products)
+		{
+			var response = await _dbContext.Products.AsNoTracking().FirstOrDefaultAsync(x=>x.Id == products.Id);
+			if (response is not null)
+			{
+				_dbContext.Products.Update(products);
+				await _dbContext.SaveChangesAsync();
+				return Ok();
+			}
+			return NotFound();
+		}
+
+		[HttpDelete("{id:Guid}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult<Products>> Delete(Guid id)
+		{
+			var product = _dbContext.Products.Find(id);
+			if (product is not null)
+			{
+				_dbContext.Products.Remove(product);
+				await _dbContext.SaveChangesAsync();
+				return Ok();
+			}
+			return NotFound();
+		}
+	}
+}
---
+++ Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Model;
+
+namespace Store.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+		public DbSet<Products> Products { get; set; }
+	}
+}
